aa558b6359b34f12499e2356335c942f124764de
Nick <nick.alex.ritchie@gmail.com>
2021-07-01 17:09:42 +1000

    starting to fill automatically

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 timeBlockContElement = $('#time_block_container');
+hoursDict = {1: '9', 2: '10', 3: '11', 4: '12', 5: '1', 6: '2', 7: '3', 8: '4', 9: '5'};
+hoursList = ['9', '10', '11', '12', '1', '2', '3', '4', '5'];
 
-// helper functions
+
+// || HELPER FUNCTIONS
+// makes a jquery element
 function makeNewJqueryElement(elementType, classString, idString){
     let newElement = $('<'+elementType+'>');
     if(classString){
@@ -10,6 +14,24 @@ function makeNewJqueryElement(elementType, classString, idString){
         newElement.attr('id', idString);
     }
     return newElement;
+}
+// gets current hour as integer
+function currentHour(){
+    currentMoment = moment();
+    let currentHour = parseInt(currentMoment.format('h'), 10);
+    return currentHour
+}
+// function to swap index number to a base 12 counter
+function getTimeSideFromIndex(index){
+    let am='PM';
+    if (index < 11){
+        am = 'AM';
+    }
+    return am;
+}
+function getTimeFromIndex(index){
+    let hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    return hours[index];
 }
 
 // function to populate 1 day of work sessions
@@ -40,4 +62,66 @@ function addTimeBlockToPage(time, period, text){
 
 }
 
-addTimeBlockToPage(3, 'present', 'bla bla text');
+// function to populate an entire day - first 9 elements of indexToHour
+function populateFullDay(){
+    let moment = currentHour();
+    // loop through hours 9AM to 5PM
+    for(let i=0; i <= hoursList.length; i++){
+        let hour = hoursList[i];
+        let am = getTimeSideFromIndex();
+        let period = 'past';
+        // case for present
+        if(hour===moment){
+            period = 'present';
+        // override for future
+        } else if(hour > moment){
+            period = 'future';
+        } 
+        addTimeBlockToPage(hour+am, period, 'blank');
+    }
+}
+class MemoryManager{
+    // construct score with initials: value
+    constructor(){
+        this.memoryName = 'userDayLog';
+        this.details = [];
+    }
+
+    // function that loads all details from storage
+    load(){
+        // reset details
+        this.details = [];
+        // get results from the save store
+        let memoryAsString = localStorage.getItem(this.memoryName);
+        let loadedResults = JSON.parse(memoryAsString);
+        if (loadedResults){
+            // add the results from local storage to results list
+            this.results = loadedResults;
+        } else {
+            // if object has no records in memory
+            if (debug){
+                console.log('No result found in local storage');
+            }
+        }
+    }
+
+    // saves details to local storage
+    save(){
+        localStorage.setItem(this.memoryName, JSON.stringify(this.details));
+    }
+
+    // edit a specific detail
+    updateDetail(index, text){
+        self.details[index] = text;
+    }
+
+    // reset local memory
+    resetLocalMemory(){
+        localStorage.clear();
+        console.log('reset local storage was complete');
+    }
+
+
+}
+
+populateFullDay();
